a7db58a7ec4da775d0eedc59886f7e393439f051
JorikDura <vandarkslave@outlook.com>
2024-10-21 01:48:18 +0300

    removed action

--- app/Http/Controllers/Api/V1/Auth/DeleteUserImageController.php
+++ app/Http/Controllers/Api/V1/Auth/DeleteUserImageController.php
@@ -4,26 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Actions\Images\DeleteImageAction;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Container\Attributes\CurrentUser;
-use ReflectionException;
 use Symfony\Component\HttpFoundation\Response;
 
 class DeleteUserImageController extends Controller
 {
     /**
      * @param  User  $user
-     * @param  DeleteImageAction  $action
      * @return Response
-     * @throws ReflectionException
      */
-    public function __invoke(
-        #[CurrentUser] User $user,
-        DeleteImageAction $action
-    ): Response {
-        $action($user);
+    public function __invoke(#[CurrentUser] User $user): Response
+    {
+        $user->deleteImage();
 
         return response()->noContent();
     }
--- app/Http/Controllers/Api/V1/Auth/UploadUserImageController.php
+++ app/Http/Controllers/Api/V1/Auth/UploadUserImageController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Actions\Images\DeleteImageAction;
 use App\Actions\Images\StoreImageAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Auth\UploadUserImageRequest;
@@ -19,17 +18,15 @@ class UploadUserImageController extends Controller
      * @param  User  $user
      * @param  UploadUserImageRequest  $request
      * @param  StoreImageAction  $storeImageAction
-     * @param  DeleteImageAction  $deleteImageAction
      * @return ImageResource
      * @throws ReflectionException
      */
     public function __invoke(
         #[CurrentUser] User $user,
         UploadUserImageRequest $request,
-        StoreImageAction $storeImageAction,
-        DeleteImageAction $deleteImageAction
+        StoreImageAction $storeImageAction
     ): ImageResource {
-        $deleteImageAction($user);
+        $user->deleteImage();
 
         $image = $storeImageAction->store(
             file: $request->validated('image'),
